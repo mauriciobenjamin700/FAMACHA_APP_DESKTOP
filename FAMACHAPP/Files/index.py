2a139ec52270e8ca065d0f42c974b4f57c189381
LazimR <lrodrigues356@gmail.com>
2023-12-10 14:22:32 -0300

    Seletor de diretorios e botão sair funcionais

--- FAMACHAPP/Files/index.py
+++ FAMACHAPP/Files/index.py
@@ -9,12 +9,37 @@ from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.popup import Popup
+import os
 
 class Index(Widget):
-    pass 
-        
-class Analise(AnchorLayout):
-    pass
+    def stop_app(self):
+        App.get_running_app().stop()
+
+class Analise(BoxLayout):
+    def open_file_chooser(self,title,dirselect):
+        self.popup_title = title
+        self.initial_path = os.path.join(os.path.expanduser('~'),'Pictures')
+        file_chooser_popup = FileChooserPopup(callback=self.file_selected,title=self.popup_title,initial_path=self.initial_path,dirselect=dirselect)
+        file_chooser_popup.open()
+
+    def file_selected(self, file_path):
+        print("Arquivo selecionado na função principal:", file_path)
+
+class FileChooserPopup(Popup):
+    def __init__(self, callback,title,dirselect=False,initial_path = '', **kwargs):
+        super(FileChooserPopup, self).__init__(**kwargs)
+        self.title = title
+        self.file_chooser = FileChooserListView(path=initial_path,dirselect=dirselect)
+        self.file_chooser.bind(on_submit=self.file_selected)
+        self.content = self.file_chooser
+        self.callback = callback
+
+    def file_selected(self, instance, selection, touch=None):
+        print("Arquivo selecionado:", selection[0])
+        self.callback(selection[0])  # Chama a função de retorno com o caminho do arquivo
+        self.dismiss()  # Fecha o popup
 
 class FamachApp(App):
     def build(self):
